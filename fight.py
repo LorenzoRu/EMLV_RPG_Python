e6593ae8280e6865d2a8d55288e32abaca59b3d8
lorenzo <lorenzo.ruocco@gmail.com>
2024-02-14 21:20:56 +0100

    ajout consomable à fix affichage de ATK après utilisation des objet

--- fight.py
+++ fight.py
@@ -51,15 +51,18 @@ def open_inventory():
         textEffect(f"[{item[0]}] {item[1]} : {item[2]}")
 
 
+
 def fight():
     monster = random_monster()
     hp_hero = stats[2]
+    attack_hero = stats[3]
+    attack_monster = monster[2]
     hp_monster = monster[3]
     print(f"{monster[1]} vous attaque !")
     while hp_hero > 0 and hp_monster > 0:
         print("--- Infos sur le combat ---")
-        textEffect(f"{player[1]} : {hp_hero}")
-        textEffect(f"{monster[1]} : {hp_monster}")
+        textEffect(f"{player[1]} : \n PV :{hp_hero} \n ATK : {attack_hero}")
+        textEffect(f"{monster[1]} : \n PV :{hp_monster} \n ATK : {attack_monster}")
         print("---------------------------")
         textEffect("Que voulez-vous faire ?")
         textEffect("[1] Attaquer")
@@ -70,6 +73,38 @@ def fight():
             print(f"Vous attaquez {monster[1]} !")
             break
         elif choice == "2":
+            open_inventory()
+            textEffect("Quel objet voulez-vous utiliser ?")
+            item_choice = input()
+            item_found = False
+            for item in list_inventory:
+                if item_choice == str(item[0]):
+                    item_found = True
+                    while True:
+                        if item_choice == "1":
+                            textEffect("Vous essayer de corrompre votre opposant avec votre fortune mais rien ne se passe !")
+                            textEffect("Quel objet voulez-vous utiliser ?")
+                            item_choice = input()
+                        elif item_choice == "2":
+                            print("2")
+                            break
+                        elif item_choice == "3": 
+                            textEffect(f"Vous utlilisez {item[2]}, en suivant les précaution de votre druide préféré votre attack augmente de 5")
+                            connection = sqlite3.connect('Database/classes.db')
+                            cusor = connection.cursor()
+                            cusor.execute("UPDATE classes SET  attack = attack + 5 WHERE id=?", (player[2],))
+                            break
+                        elif item_choice == "4":
+                            print("4")
+                            break
+                        else:
+                            textEffect("Quel objet voulez-vous utiliser ?")
+                            textEffect("Tu es créatif mais je ne suis pas sûr que cette qualité t'aide")
+                            textEffect("Quel objet voulez-vous utiliser ?")
+                            item_choice = input()
+            if not item_found:
+                textEffect("Cet objet n'est pas dans votre inventaire !")
+        elif choice == "3":
             print("Vous fuyez le combat...")
             break
         else:
